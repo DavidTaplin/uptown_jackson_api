ada14f665484d867319aee0bb5ec3458d29519b1
DavidTaplin <DTaplin616@live.com>
2023-05-04 16:58:59 -0500

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,23 @@
 class UsersController < ApplicationController
 
-def new 
-    @user = User.new
-end
+# def new 
+ #   user = User.new
+# end
 
-def create 
-    @user = User.new(user_params)
-    if @user.save
-        redirect_to root_path, notice: 'User was successfully created.'
-    else 
-        render :new
+def create
+    user = User.new(user_params)
+    if user.save
+     render json: user, status: :created
+    else
+     render json: user.error, status: :unprocessable_entity
     end
-end
+   end
+  
 
-private
+# private
 
 def user_params
-    params.require(:user).permit(:email, :first_name, :last_name, :password)
+    params.require(:user).permit(:email, :first_name, :last_name, :password_digest)
+end
+
 end
