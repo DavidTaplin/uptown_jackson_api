a587eb664273de8f0c60ae734d3d914660de98b4
josephwwilliams <josephwwilliams18@gmail.com>
2023-05-15 02:04:57 -0500

    ADD: Auth Tokens

--- config/routes.rb
+++ config/routes.rb
@@ -9,16 +9,6 @@ Rails.application.routes.draw do
     delete :destroy
   end
 
-  get 'buildings/show/:id', to: 'buildings#show', as: 'show'
-
-  # resources :users
-  namespace :users, defaults: {format: 'json'} do
-    post :create
-  end
-  
-  resources :sessions
-  #create '/login', to: 'sessions#create'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  devise_for :users
 
 end
