e07290956e98a0450e4c3bbce76abf92865bd545
tguardado1994 <111324014+tguardado1994@users.noreply.github.com>
2023-05-24 20:48:35 -0500

    edit

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,6 +1,6 @@
 class BuildingsController < ApplicationController
   skip_before_action :verify_authenticity_token, raise: false
-  before_action :authenticate_devise_api_token!, only: [:create, :destroy]
+  before_action :authenticate_devise_api_token!, only: [:create, :destroy, :update]
 
 
   def create
